09bd78e38817c1641a1143a0bc0208a836681ba8
komalgs11 <komalpatil26.kp@gmail.com>
2023-08-04 20:17:42 +0200

    updated movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +10,24 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.Title}
     </div>
   );
+};
+// Here is where we define all the props constraints for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    _id: propTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string.isRequired,
+    Genre: propTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }).isRequired,
+    Director: propTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Born: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+    }).isRequired,
+    Description: PropTypes.string.isRequired,
+    Featured: PropTypes.string.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
