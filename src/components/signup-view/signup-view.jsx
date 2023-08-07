a8c12c338541e737ce1e4998945b5e1891ebe3fc
komalgs11 <komalpatil26.kp@gmail.com>
2023-08-07 23:07:50 +0200

    created signup-view folder and jsx

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+export const SignupView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthdate, setBirthDate] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      BirthDate: birthdate
+    };
+    fetch("SIGNUP_URL", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (response.ok) {
+        alert("SignUp Successful");
+        window.location.reload();
+      } else {
+        alert("SignUp failed");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Birthday:
+        <input
+          type="date"
+          value={birthdate}
+          onChange={(e) => setBirthDate(e.target.value)}
+          required
+        />
+      </label>
+      <button type="Submit">SignUp</button>
+    </form>
+  );
+};
